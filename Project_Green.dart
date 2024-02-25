24bd38ae0d338f48ea64c5eb816f80e70ec6c13e
ABIR ROY <72157530+abhuman@users.noreply.github.com>
2024-02-25 12:00:21 +0530

    Create Project_Green.dart

---
+++ Project_Green.dart
@@ -0,0 +1,360 @@
+import '/flutter_flow/flutter_flow_checkbox_group.dart';
+import '/flutter_flow/flutter_flow_count_controller.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'project_green_model.dart';
+export 'project_green_model.dart';
+
+class ProjectGreenWidget extends StatefulWidget {
+  const ProjectGreenWidget({super.key});
+
+  @override
+  State<ProjectGreenWidget> createState() => _ProjectGreenWidgetState();
+}
+
+class _ProjectGreenWidgetState extends State<ProjectGreenWidget> {
+  late ProjectGreenModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ProjectGreenModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+        appBar: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+          automaticallyImplyLeading: false,
+          leading: FlutterFlowIconButton(
+            borderColor: Colors.transparent,
+            borderRadius: 30,
+            borderWidth: 1,
+            buttonSize: 60,
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: FlutterFlowTheme.of(context).secondaryText,
+              size: 30,
+            ),
+            onPressed: () {
+              print('IconButton pressed ...');
+            },
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 0,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            'https://media.istockphoto.com/id/463116677/photo/silhouette-of-helping-hand-between-two-climber.jpg?s=170667a&w=0&k=20&c=UYISNOTuUsr4ef5d5eASiVLulB3UuPGQfOpfuvC1t4w=&auto=format&fit=crop&w=3437&q=80',
+                            width: double.infinity,
+                            height: 230,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 4, 0, 0),
+                        child: Text(
+                          'Project Green',
+                          style: FlutterFlowTheme.of(context).headlineMedium,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 4, 0, 0),
+                        child: Text(
+                          '₹ 100',
+                          style: FlutterFlowTheme.of(context).labelLarge,
+                        ),
+                      ),
+                      Divider(
+                        height: 20,
+                        thickness: 2,
+                        indent: 16,
+                        endIndent: 16,
+                        color: FlutterFlowTheme.of(context).alternate,
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                        child: Text(
+                          'Description',
+                          style: FlutterFlowTheme.of(context).labelMedium,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 4, 16, 0),
+                        child: Text(
+                          'You can Choose Project Green to make a smile in 90 people face ',
+                          style: FlutterFlowTheme.of(context).bodyLarge,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 8),
+                              child: Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                alignment: AlignmentDirectional(-0.9, 0),
+                                child: Text(
+                                  'Type of Food',
+                                  style:
+                                      FlutterFlowTheme.of(context).labelMedium,
+                                ),
+                              ),
+                            ),
+                            FlutterFlowCheckboxGroup(
+                              options: ['Veg', 'Non Veg'],
+                              onChanged: (val) => setState(
+                                  () => _model.checkboxGroupValues1 = val),
+                              controller:
+                                  _model.checkboxGroupValueController1 ??=
+                                      FormFieldController<List<String>>(
+                                [],
+                              ),
+                              activeColor: FlutterFlowTheme.of(context).primary,
+                              checkColor: Colors.white,
+                              checkboxBorderColor:
+                                  FlutterFlowTheme.of(context).secondaryText,
+                              textStyle:
+                                  FlutterFlowTheme.of(context).bodyMedium,
+                              itemPadding:
+                                  EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                              checkboxBorderRadius: BorderRadius.circular(4),
+                              initialized: _model.checkboxGroupValues1 != null,
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 8),
+                              child: Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                alignment: AlignmentDirectional(-0.9, 0),
+                                child: Text(
+                                  'T shirt Size',
+                                  style:
+                                      FlutterFlowTheme.of(context).labelMedium,
+                                ),
+                              ),
+                            ),
+                            FlutterFlowCheckboxGroup(
+                              options: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                              onChanged: (val) => setState(
+                                  () => _model.checkboxGroupValues2 = val),
+                              controller:
+                                  _model.checkboxGroupValueController2 ??=
+                                      FormFieldController<List<String>>(
+                                [],
+                              ),
+                              activeColor: FlutterFlowTheme.of(context).primary,
+                              checkColor: Colors.white,
+                              checkboxBorderColor:
+                                  FlutterFlowTheme.of(context).secondaryText,
+                              textStyle:
+                                  FlutterFlowTheme.of(context).bodyMedium,
+                              itemPadding:
+                                  EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                              checkboxBorderRadius: BorderRadius.circular(4),
+                              initialized: _model.checkboxGroupValues2 != null,
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 8),
+                              child: Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                alignment: AlignmentDirectional(-0.9, 0),
+                                child: Text(
+                                  'FOOD',
+                                  style:
+                                      FlutterFlowTheme.of(context).labelMedium,
+                                ),
+                              ),
+                            ),
+                            FlutterFlowCheckboxGroup(
+                              options: ['Iced', 'Hot'],
+                              onChanged: (val) => setState(
+                                  () => _model.checkboxGroupValues3 = val),
+                              controller:
+                                  _model.checkboxGroupValueController3 ??=
+                                      FormFieldController<List<String>>(
+                                [],
+                              ),
+                              activeColor: FlutterFlowTheme.of(context).primary,
+                              checkColor: Colors.white,
+                              checkboxBorderColor:
+                                  FlutterFlowTheme.of(context).secondaryText,
+                              textStyle:
+                                  FlutterFlowTheme.of(context).bodyMedium,
+                              itemPadding:
+                                  EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                              checkboxBorderRadius: BorderRadius.circular(4),
+                              initialized: _model.checkboxGroupValues3 != null,
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 12),
+                              child: Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                alignment: AlignmentDirectional(-0.9, 0),
+                                child: Text(
+                                  'Quanity',
+                                  style:
+                                      FlutterFlowTheme.of(context).labelMedium,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
+                              child: Container(
+                                width: 160,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: FlutterFlowTheme.of(context)
+                                      .secondaryBackground,
+                                  borderRadius: BorderRadius.circular(25),
+                                  shape: BoxShape.rectangle,
+                                  border: Border.all(
+                                    color:
+                                        FlutterFlowTheme.of(context).alternate,
+                                    width: 1,
+                                  ),
+                                ),
+                                child: FlutterFlowCountController(
+                                  decrementIconBuilder: (enabled) => Icon(
+                                    Icons.remove_rounded,
+                                    color: enabled
+                                        ? FlutterFlowTheme.of(context)
+                                            .secondaryText
+                                        : FlutterFlowTheme.of(context)
+                                            .alternate,
+                                    size: 20,
+                                  ),
+                                  incrementIconBuilder: (enabled) => Icon(
+                                    Icons.add_rounded,
+                                    color: enabled
+                                        ? FlutterFlowTheme.of(context).primary
+                                        : FlutterFlowTheme.of(context)
+                                            .alternate,
+                                    size: 20,
+                                  ),
+                                  countBuilder: (count) => Text(
+                                    count.toString(),
+                                    style: FlutterFlowTheme.of(context)
+                                        .headlineSmall,
+                                  ),
+                                  count: _model.countControllerValue ??= 1,
+                                  updateCount: (count) => setState(() =>
+                                      _model.countControllerValue = count),
+                                  stepSize: 1,
+                                  minimum: 1,
+                                  maximum: 10,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
+                child: FFButtonWidget(
+                  onPressed: () {
+                    print('Button pressed ...');
+                  },
+                  text: 'Add To Cart',
+                  options: FFButtonOptions(
+                    width: double.infinity,
+                    height: 50,
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    color: FlutterFlowTheme.of(context).primaryText,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Readex Pro',
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                    elevation: 3,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
